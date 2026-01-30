1f6ce168ffb9499de48d834a007a50c853df729a
rakeshchaure <95487917+rakeshchaure@users.noreply.github.com>
2026-01-30 15:51:57 +0530

    Assignment 4 Task 2

---
+++ Assignement4_task2.py
@@ -0,0 +1,18 @@
+import os
+a=input("Enter text to write to the file:")
+
+with open("output.txt","wt") as fh:
+    fh.write(a+"\n")
+print("\n Data successfully written to output.txt")
+
+b=input("Enter additional text to append:")
+
+with open("output.txt","at") as fh:
+    fh.write(b +"\n")
+
+print("\n Data Successfully appended.\n")
+
+print("Final Content of output.txt:\n")
+
+with open("output.txt","rt") as fh:
+    print(fh.read())
