45549f934ccf379a056139def272b4aa369e10dc
rakeshchaure <95487917+rakeshchaure@users.noreply.github.com>
2026-02-04 11:55:01 +0530

    Refactor file handling modes for clarity

--- Assignement4_task2.py
+++ Assignement4_task2.py
@@ -1,18 +1,19 @@
 import os
 a=input("Enter text to write to the file:")
 
-with open("output.txt","wt") as fh:
+with open("output.txt","w") as fh:
     fh.write(a+"\n")
 print("\n Data successfully written to output.txt")
 
 b=input("Enter additional text to append:")
 
-with open("output.txt","at") as fh:
+with open("output.txt","a") as fh:
     fh.write(b +"\n")
 
 print("\n Data Successfully appended.\n")
 
 print("Final Content of output.txt:\n")
 
-with open("output.txt","rt") as fh:
+with open("output.txt","r") as fh:
     print(fh.read())
+
